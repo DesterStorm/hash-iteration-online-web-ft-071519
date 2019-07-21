50adb4519179f23f7e515cbbbf9293c675579bf6
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-07-21 20:02:53 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,6 +11,8 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
-  birthday_kids.each do |age| age > 12 puts "You are too old for this." 
+  birthday_kids.each do |age| age > 12 
+end
+  if age > 12 puts "You are too old for this." 
   
 end
